a2f197ad7b41a66867c9a7843a7fdc7f1923b7f2
Guillaume Dumont <dumont.guillaume@gmail.com>
2017-01-12 16:45:48 -0500

    Repleace hard-coded path to zlib in hdf5-targets.cmake with relative path.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,16 +2,22 @@ mkdir build
 cd build
 
 REM set environement variables
-set HDF5_EXT_ZLIB=zlib.lib
+set HDF5_EXT_ZLIB=z.lib
 
 REM configure step
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE:STRING=RELEASE -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% -DHDF5_BUILD_CPP_LIB=ON -DBUILD_SHARED_LIBS:BOOL=ON -DHDF5_BUILD_HL_LIB=ON -DHDF5_BUILD_TOOLS:BOOL=ON -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON %SRC_DIR%
 if errorlevel 1 exit 1
 
 REM Build C libraries and tools
-nmake
+cmake --build .
 if errorlevel 1 exit 1
 
 REM Install step
-nmake install
+cmake --build . --target install
+
+REM Patch the cmake targets to avoid hard-coded path to z.lib
+"%PYTHON%" "%RECIPE_DIR%\patch_cmake_targets.py" ^
+           "%LIBRARY_PREFIX%\cmake\hdf5-targets.cmake" ^
+           "%LIBRARY_PREFIX:\=/%/lib/z.lib" ^
+           "${_IMPORT_PREFIX}/lib/z.lib"
 if errorlevel 1 exit 1
